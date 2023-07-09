fd3337d1bfdc3da9e51591d13f02ec95d4123f50
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-07-09 14:13:13 +0330

    :recycle: refactor: modify api routes

--- app/api/accounts/route.ts
+++ app/api/accounts/route.ts
@@ -3,7 +3,7 @@ import { sql } from "@vercel/postgres";
 export const GET = async () => {
 
   try {
-    const account = await sql`SELECT * FROM accounts;`;
+    const account = await sql`SELECT * FROM "Account";`;
     return new Response(JSON.stringify({ account }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ error }), { status: 500 });
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres";
 
 export const GET = async () => {
   try {
-    const users = await sql`SELECT * FROM users;`;
+    const users = await sql`SELECT * FROM "User";`;
     return new Response(JSON.stringify({ users }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ error }), { status: 500 });
